56deefe0343489c7233cd74488587c393c059a9f
Alvin Siew <alvin_siew@tech.gov.sg>
2019-06-29 22:53:21 +0800

    Added new feature to add and remove host from ssh conf

--- slist.sh
+++ slist.sh
@@ -14,8 +14,10 @@ config_file=~/.ssh/config
 red=$'\e[1;31m'
 end=$'\e[0m'
 
+# Function for printing usage
 usage() { echo "Usage: $0 [-p <filer word>]" 1>&2; exit 1; }
 
+# Function for printing help page
 help() {
 cat << EOF
 Usage: slist [-hl]|[-f <keyword>]
@@ -25,11 +27,19 @@ Usage: slist [-hl]|[-f <keyword>]
 -l                            List servers with ip addresses
 -l -f <keyword>               Filter list work <keyword>
 
+Adding new host to ssh config
+Usage: slist --add-host <host name> --ip-adr <ip address> --ssh-user <user> --port <port number> --keypath < keyname with path >
+
+Only --add-host and --ip-adr are mandatory to have
+
+Delete host from ssh confg
+Usage: slist --del-host <host name>
+
 EOF
 exit;
 }
 
-
+# Default main slist page
 main() {
     check_config_file_exists
     list=$(< $config_file grep "Host " | awk '{print $2}')
@@ -85,6 +95,7 @@ main() {
     exit
 }
 
+# Filter hostname page of slist
 filter() {
     list=$(< $config_file grep "Host " | awk '{print $2}')
     rm -f $list_path
@@ -142,6 +153,7 @@ filter() {
     exit
 }
 
+# Listing ip address for slist
 list() {
     colour=34
     < $config_file grep Host | while read -r line;
@@ -163,6 +175,7 @@ list() {
     exit;
 }
 
+# Filtering ip address page of slist
 flist() {
     < $config_file grep Host | while read -r line;
     do
@@ -177,36 +190,104 @@ flist() {
     exit;
 }
 
+# Function to check if config file exists
 check_config_file_exists(){
     if [ -f "$config_file" ]; then
-    echo "$config_file exist"
+        echo "$config_file exist"
     else 
 
-    printf "%s\n" "${red} $config_file <-- file does not exist - Please create one. ${end} "
-    exit;
+        printf "%s\n" "${red} $config_file <-- file does not exist - Please create one. ${end} "
+        exit;
     fi
 }
 
+# Function to check if last line of conf is empty
+check_last_line(){
+  last_line=$(tail -1 ${config_file})
+  if [ ! -z "$last_line" ]; then
+      echo "" >> "$config_file"
+  fi
+}
+
+# Function to check if hostname already exists
+check_host_exists(){
+  host_exist=false
+  match=$1
+  host_check=$(sed '/^Host/!d' $config_file | awk "/$match/ "'{ print $2 }')
+  for line in $host_check; do
+      if [[ $line == $match ]]; then
+
+          host_exist=true
+          break
+      fi
+  done
+}
+
+# Loading main slist page if no argument found
 if [ $# -eq 0 ]; then
     clear
     main
 fi
 
-list=no
-filter=no
-help=no
+# Start of slist
+list=false
+filter=false
+help=false
+add_host=false
+ip_adr=false
+del_host=false
 
-while getopts ':hlf:' c
+# Not mandatory fields. Will use default or no value if bolean is false
+ssh_user=false
+port=false
+keypath=false
+
+while getopts ':hlf:-:' c
 do
   case $c in
+    -)
+      case "$OPTARG" in
+        add-host)
+          add_host=true
+          val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          host=$val
+          ;;
+        ip-adr)
+          ip_adr=true
+          val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          ip="$val"
+          ;;
+        ssh-user)
+          ssh_user=true
+          val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          user="$val"
+          ;;
+        port)
+          add_port=true
+          val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          port="$val"
+          ;;
+        keypath)
+          key_path=true
+          val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          key="$val"
+          ;;
+        del-host)
+          del_host=true
+          val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+          host="$val"
+          ;;
+        *)
+          echo "Illegal option --$OPTARG" >&2; exit 2 ;;
+      esac;;
     h)
-      help=yes
+      help=true
       ;;
     l)
-      list=yes
+      list=true
       ;;
     f)
-      filter=yes
+      filter=true
       keyword=$OPTARG
       ;;
     *)
@@ -216,17 +297,67 @@ do
       ;;
   esac
 done
-
-if [[ $help == "yes" ]]; then
+ 
+if [[ $help == "true" ]]; then
     help
-elif [[ $list == "yes" ]] && [[ $filter == "no" ]] && [[ $help == "no" ]]; then
+elif [[ $add_host == "true" ]] && [[ $del_host == "true" ]]; then
+    echo "--add-host and --del-host cannot be use at the same time"
+    exit 3
+elif [[ $del_host == "true" ]]; then
+    check_host_exists "$host"
+    if [[ $host_exist == "false" ]]; then
+        echo "Host does not exist"
+        exit 0
+    fi
+    echo -e "\nDelete host ${host}? [y/n]"
+    read -r confirm
+    if [[ $confirm == "y" ]] || [[ $confirm == "yes" ]]; then
+        sed -i.bak "/Host $host/",'/^$/d' config
+        if [ $? -ne 0 ]; then
+            echo "Host could not be remove. Please do manual removal from ~/.ssh/config."
+            exit 3
+        fi
+        echo "${host} had been removed" 
+        exit 0
+    fi
+elif [[ $add_host == "true" ]]; then
+    check_host_exists "$host"
+    # Check if host already exist
+    if [[ $host_exist == "true" ]]; then
+        echo "Host with the same name already exist"
+        exit 0
+    fi
+    # Prompt for ip address argument was not pass
+    if [[ $ip_adr == "false" ]]; then
+        echo -e "\nEnter IP address or hostname for host ${host}"
+        read -r ip
+    fi
+    # Update config file
+    if [[ $host_exist == "false" ]]; then
+        check_last_line
+        echo "Host ${host}" >> "$config_file"
+        if [[ $ssh_user == "true" ]]; then
+            echo "  User ${user}" >> "$config_file"
+        fi
+        echo "  Hostname ${ip}" >> "$config_file"
+        if [[ $key_path == "true" ]]; then
+            echo "  IdentityFile ${key}" >> "$config_file"
+        fi
+        if [[ $add_port == "true" ]]; then
+            echo "  Port ${port}" >> "$config_file"
+        fi
+    fi
+# Logic ensure that options ip-adr, ssh-user, port and keypath should not be call without add-host
+elif [[ $add_host == "false" ]] && [[ $ip_adr == "true" ]] || [[ $ssh_user == "true" ]] || [[ $add_port == "true" ]] || [[ $key_path == "true" ]]; then
+    help
+elif [[ $list == "true" ]] && [[ $filter == "false" ]] && [[ $help == "false" ]]; then
     clear
     list
     exit
-elif [[ $filter == "yes" ]] && [[ $list == "no" ]] && [[ $help == "no" ]]; then
+elif [[ $filter == "true" ]] && [[ $list == "false" ]] && [[ $help == "false" ]]; then
     clear
     filter
-elif [[ $filter == "yes" ]] && [[ $list == "yes" ]] && [[ $help == "no" ]]; then
+elif [[ $filter == "true" ]] && [[ $list == "true" ]] && [[ $help == "false" ]]; then
     clear
     colour=34
     flist | grep -A1 "$keyword" | grep -v "\-\-" | while read -r line;
