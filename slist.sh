19910311d9d68e46fddde04e717bf0119b0f4511
Alvin Siew <alvin_siew@tech.gov.sg>
2019-07-18 11:23:02 +0800

    fix linting

--- slist.sh
+++ slist.sh
@@ -158,7 +158,7 @@ list() {
     < $config_file grep Host | while read -r line;
     do
         if [[ $line != *"*"* ]]; then
-            line="$(tr 'A-Z' 'a-z' <<< "$line")"
+            line="$(tr '[:upper:]' '[:lower:]' <<< "$line")"
             if [[ $line == *"host "* ]]; then
                 replace_string=$(sed 's/host/Server:/g' <<< "$line")
                 if [ $colour -eq 34 ]; then
@@ -179,7 +179,7 @@ list() {
 flist() {
     < $config_file grep Host | while read -r line;
     do
-        line="$(tr '[A-Z]' '[a-z]' <<< "$line")"
+        line="$(tr '[:upper:]' '[:lower:]' <<< "$line")"
         if [[ $line == *"host "* ]]; then
             replace_string=$(sed 's/host/Server:/g' <<< "$line")
         elif [[ $line == *"hostname "* ]]; then
